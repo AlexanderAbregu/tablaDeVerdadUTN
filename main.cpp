58a71a05c429af2e12655c8adb9c91eb7ea1c468
Alexander Abregu <alexanderarmua@hotmail.com>
2016-07-10 22:40:15 -0300

    Maquillado

--- main.cpp
+++ main.cpp
@@ -9,6 +9,23 @@
 
 using namespace std;
 
+//Cambia el color de las letras.
+void color( int X ){ 
+	SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),X); 
+	/* 
+		0 = Negro 
+		1 = Azul 
+		2 = Verde 
+		3 = Aguamarina 
+		4 = Rojo 
+		5 = Purpura 
+		6 = Amarillo 
+		7 = Blanco 
+		8 = Gris 
+		9 = Azul claro 
+	*/
+}
+
 /*
 	Para rellenar la parte de una tabla de verdad que contiene las proposiciones simples. 
 		Ejemplo: 
@@ -192,11 +209,24 @@ string tipoProposicion ( bool *columnaFinal, int cantidadFilas){
 }
 
 int main(int argc, char **argv){
+	color(8);
+	cout<<"\t Proposicion Compuesta.\n";
+	cout<<"\t ----------------------\n\n";
+	cout<<"Consigna:\n";
+	cout<<"---------\n";
+	cout<<"\t**************************************************************************************\n";
+	cout<<"\t* Generar un programa donde dada una proposicion compuesta (de al menos 3 terminos), *\n";
+ 	cout<<"\t* se indique si se trata de una tautologia, contradiccion o contingencia.            *\n";
+ 	cout<<"\t* Las interfaces y los metodos de entrada del programa seran definidos por el grupo. *\n";
+ 	cout<<"\t**************************************************************************************\n\n";
+	
 	int cantProp = 0;
 	
 	do{
 		cout << " Cantidad de proposiciones simples ( Mayor o igual que 3 y menor o igual a 5): ";
+		color(7);
 		cin >> cantProp;
+		color(8);
 	}while( ( cantProp < 3 ) || ( cantProp > 5 ) );
 	
 	// La cantidad de combinaciones que se pueden dar es igual a  2 elevado Cantidad de Proposiciones.
@@ -228,17 +258,34 @@ int main(int argc, char **argv){
 	// Para representar en la consola con simbolos similares a los utilizados en Matematica Discreta.
 	string operadoresLogicos[] = { "^", "v", "=>", "<=>", "_V_"};
 	
-	cout << "\n Operadores Logicos: \n";
+	color(3);
+	cout << "\n \tOperadores Logicos: \n";
+	cout << "\t-------------------\n";
 	for ( int i = 0; i < ARRAY_SIZE(operadoresLogicos); i++){
-		cout << "\n\t Operador " << operadoresLogicos[i] << "\t: " << ( i + 1);
+		cout << "\n\t\t Operador " << operadoresLogicos[i] << "\t: "; 
+		color(1); 
+		cout << ( i + 1 ); 
+		color(3);
 	}
+	cout << endl;
 	
 	string textoOp1 = string( 1, proposiciones[0] );
 	string textoOp2 = string( 1, proposiciones[1] );
 	
 	for ( int i = 0; i < cantidadOperadores; i++ ){
-		cout << "\n Que operador logico desea poner entre ( " << textoOp1 << " y " << textoOp2 << " )?: ";
+		cout << "\n Que operador logico desea poner entre ( ";
+		color(1);
+		cout << textoOp1;
+			color(9);
+			cout << " y ";
+			color(1);
+			cout << textoOp2;
+		color(3);
+		cout << " )?: ";
+		
+		color(1);
 		cin >> operadores[i];
+		color(3);
 		
 		textoOp1 = "( " + textoOp1 + " " + operadoresLogicos[operadores[i] - 1] + " " +textoOp2 + " )";
 		
@@ -246,31 +293,36 @@ int main(int argc, char **argv){
 			textoOp2 = proposiciones[i + 2];
 		}
 	}
-		
-	cout << "\n Resultado = " << textoOp1 << endl;
 	
-	int valor = 0;
+	cout << "\n\n\tOperacion compuesta resultante: "; color(6); cout << textoOp1 << endl; color(3);
+	cout << "\t-------------------------------" << endl;
+	
+	int indice = 0;
 	
 	for ( int i = 0; i < cantidadOperadores; i++ ){
-		llenarColumna( tablaDeVerdad[valor], tablaDeVerdad[i + 1], tablaDeVerdad[cantProp + i], operadores[i], cantFilas );
+		llenarColumna( tablaDeVerdad[indice], tablaDeVerdad[i + 1], tablaDeVerdad[cantProp + i], operadores[i], cantFilas );
 		
 		if ( i == 0){
-			valor = cantidadOperadores;
+			indice = cantidadOperadores;
 		}
 		
-		valor++;
+		indice++;
 	}
 	
+	cout << "\n\n";
+	
 	for ( int y = 0; y < cantFilas; y++ ){
+		cout << "\t\t";
 		for ( int x = 0; x < ( ( ( 2 * cantProp ) - 1 ) ); x++){
-			cout << tablaDeVerdad[x][y] << " | ";
+			cout << " | " << tablaDeVerdad[x][y] << " | ";
 		}
 		cout << endl;
 	}
 	
-	cout << "\n La proposiscion es una " << tipoProposicion( tablaDeVerdad[6], cantFilas ) << endl,
+	cout << "\n La proposiscion es una " << tipoProposicion( tablaDeVerdad[6], cantFilas ) << "." <<endl,
 	
-	cout << "\n\nFINNNNNNNNNNNNNNN" <<endl;
+	color(8);
+	cout << "\n\n(Las primeras " << cantProp << " columnas corresponden a las proposiciones simples y la ultima columna corresponde a " << textoOp1 << " ). " << endl;
 	system("pause");
 	return 0;
 }
